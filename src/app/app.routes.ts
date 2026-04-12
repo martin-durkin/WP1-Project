95c78875fca84e1f37499a8b9428a27c1ede3851
Martin Durkin - STUDENT <S00236994@atu.ie>
2026-04-12 15:56:27 +0100

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Favourites } from './favourites/favourites';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'favourites', component: Favourites },
+  { path: 'about', component: About },
+];
