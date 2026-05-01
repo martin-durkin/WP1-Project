8ff202324c449d8372354e2754a807ca1f48388b
Martin Durkin - STUDENT <S00236994@atu.ie>
2026-05-01 22:05:03 +0100

    app deployed

--- src/app/booksapi.ts
+++ src/app/booksapi.ts
@@ -1,27 +1,26 @@
 import { Injectable, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Book } from './models/book';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Booksapi {
-  private _apiUrl = 'http://localhost:5050/books';
+  private _apiUrl = environment.apiUrl;
   private _http = inject(HttpClient);
 
   items = signal<Book[]>([]);
   favourites = signal<Book[]>([]);
 
-  // search books from Open Library via Express
-   getItems(title: string, page: number = 1) {
-  const url = `${this._apiUrl}/search?title=${title}&page=${page}`;
-  this._http.get<any>(url)
-    .subscribe(data => {
-      this.items.set(data);
-    });
-}
+  getItems(title: string, page: number = 1) {
+    const url = `${this._apiUrl}/search?title=${title}&page=${page}`;
+    this._http.get<any>(url)
+      .subscribe(data => {
+        this.items.set(data);
+      });
+  }
 
-  // add one book to favourites
   addItem(title: string, author_name: string[], first_publish_year: string, cover_i: string, key: string) {
     const url = this._apiUrl;
     let book = { title, author_name, first_publish_year, cover_i, key };
@@ -31,18 +30,16 @@ export class Booksapi {
       });
   }
 
-  // get all favourites from MongoDB
   getFavourites() {
-    const url = 'http://localhost:5050/books';
+    const url = this._apiUrl;
     this._http.get<Book[]>(url)
       .subscribe(data => {
         this.favourites.set(data);
       });
   }
 
-  // delete book by id
   deleteItem(myId: string) {
-    const url = 'http://localhost:5050/books/' + myId;
+    const url = this._apiUrl + '/' + myId;
     this._http.delete(url)
       .subscribe(() => {
         this.getFavourites();
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = {
+  apiUrl: 'http://3.252.220.159:5050/books'
+};
